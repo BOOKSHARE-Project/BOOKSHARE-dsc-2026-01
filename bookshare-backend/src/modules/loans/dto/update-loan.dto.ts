9c8f25fc262667c279cc70733d2fdf67e186a6a3
mRn283 <gabriel32768@gmail.com>
2026-05-26 10:31:22 -0300

    Mais alterações

--- bookshare-backend/src/modules/loans/dto/update-loan.dto.ts
+++ bookshare-backend/src/modules/loans/dto/update-loan.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLoanDto } from './create-loan.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateLoanDto extends PartialType(CreateLoanDto) {}
+export class UpdateLoanDto extends PartialType(CreateLoanDto) {
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+}
